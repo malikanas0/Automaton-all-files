8baa7b34c4021cbfa8721f3e1aa8275c2aa7c3ae
Muhammad Anas <malikanassultan@gmail.com>
2024-12-05 17:32:41 +0500

    today's commit

---
+++ utlis/flightpro.js
@@ -0,0 +1,12 @@
+export const verifyalltestcases ={
+    mybookingbutton: '[class="css-1y1pb0x"]',
+    cacheacceptbtn: '#onetrust-accept-btn-handler',
+    mybookingbutton: '[class="css-1y1pb0x"]',
+    cacheacceptbtn: '#onetrust-accept-btn-handler',
+    clickonfindmybooking: '[class="b0x94f1"]',
+    email: '#email',
+    errorvalid: '#email-error',
+    ordernumber: '#orderNumber',
+    errormsg: '[class="etiHtmlContent"]',
+    clickonreturn: '[class="_7x9w550"]',
+}
